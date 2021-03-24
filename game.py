d01e023fe5c873d0604628a56b1cbbbf8723ca4f
raissaprado <raissafprado@yahoo.com>
2021-03-24 04:12:21 -0400

    Building out game logic part one

--- game.py
+++ game.py
@@ -13,11 +13,36 @@ choices = ["rock", "paper","scissors"]
 # player_choice = choices[1]
 # print("index 1 in the choice array is " + player_choice + ", which is paper")
 
-player_choice = input("choose rock , paper , or scissors: ")
+player_choice = input("Choose rock, paper, or scissors: ")
 
-print("user chose " + player_choice)
+print("user chose: " + player_choice)
 
 # this will be the AI choice -> a random pick frmo the choices array
 computer_choice = choices[randint(0, 2)]
 
 print ("computer_choice: " + computer_choice)
+
+if computer_choice == player_choice: 
+	print ("Tie")
+
+elif computer_choice == "rock":
+	if player_choice == "scissors"
+		print("You lose!")
+	else:
+		print("You win!")
+
+elif computer_choice == "paper":
+	if player_choice == "scissors"
+		print("You win!")
+	else:
+		print("You lose!")
+
+elif computer_choice == "scissors":
+	if player_choice == "paper"
+		print("You lose!")
+	else:
+		print("You win!")
+
+
+
+
