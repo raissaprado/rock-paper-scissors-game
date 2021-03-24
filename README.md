3e30d9c2032713926a94118a33e8d7124e483797
raissaprado <raissafprado@yahoo.com>
2021-03-24 19:52:14 -0400

    Updating ReadMe file and each print conditions

--- README.md
+++ README.md
@@ -1,9 +1,12 @@
-# Virtual Rock Paper Scissors Game
+# HW2 - Python
+
+# Description Project 
+Virtual Rock Paper Scissors Game, for class 1054-01-21W by Tina Vandervecht.
 
 A virtual version of rock paper scissors! Two players compete by choosing an option and comparing their choices. This game is win, lose or draw – the loser loses a life. Keep playing until one player has no lives to live.
 
 # Project roadmap file
-[Project roadmap google doc link](https://docs.google.com/document/d/1FsoeHDH37Z7h6PkfNaUKYOPBJ2JoEYYUWvXJotApjoo/edit?usp=sharing )
+[Project Roadmap Google Doc Link](https://docs.google.com/document/d/1FsoeHDH37Z7h6PkfNaUKYOPBJ2JoEYYUWvXJotApjoo/edit?usp=sharing )
 
 # License
 Copyright 2021 Raissa F. Prado
@@ -13,3 +16,13 @@ Permission is hereby granted, free of charge, to any person obtaining a copy of 
 The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+# Installation
+Copy repository from Github to preferred project location. You're good to go!
+
+# Contributing
+1. Fork it!
+2. Create your feature branch: `git checkout -b my-new-feature`
+3. Commit your changes: `git commit -am 'Add some feature'`
+4. Push to the branch: `git push origin my-new-feature`
+5. Submit a pull request :D
--- game.py
+++ game.py
@@ -1,34 +1,23 @@
-# Import packages to extend Python (just like we extend sublime, atom or VSCode)
 from random import randint
-
-# [] ==> this is an array
-# name = [value1, value 2]
-# an array is a special type of container that can hold multiple items
-# arrays are indexed (their contents are assigned a number)
-# the index always starts at zero
+	
 choices = ["rock", "paper","scissors"]
 
 player_lives = 3
 computer_lives = 3
 total_lives = 3
 
-# True or false are boolean data types
-# essentially, booleand are equivalent to an on or off switch, 1 or 0.
 player_choice = False 
 
-#define a win or lose function
 def winorlose(status):
-	# version 1 of function
-	# print("inside winorlose function; status is: ", status)
+	print("")
 	print("Keanu Reeves has spoken: YOU", status, "! Do you want a rematch?")
 	choice = input("Y / N? ")
 
 	if choice == "N" or choice == "n":
 		print("You chose to quit. Hope to see you soon!")
+		print("")
 		exit()
 	elif choice == "Y" or choice == "y":
-		#reset the player lives and computer lives
-		# and reset player choice to False, so our loop restarts
 		global player_lives
 		global computer_lives
 		global total_lives
@@ -36,47 +25,38 @@ def winorlose(status):
 		player_lives = total_lives
 		computer_lives = total_lives
 	else:
-		print("Make a valid choice: Would you like to play again? Y for yes or N for No")
-		# this might generate a bug that we need to fix later
+		print("Make a valid choice: Would you like to play again? Y for yes or N for no")
 		choice = input("Y / N? ")
 
-
-#player_choice == False
 while player_choice is False:
 	print("")
-	print("~~~~~~~~~~~~~~~~~~~~~~~~RPS GAME~~~~~~~~~~~~~~~~~~~~~~~~")
+	print("~~~~~~~~~~~~~~~~~~~~~~| RPS GAME |~~~~~~~~~~~~~~~~~~~~~~")
 	print("Matrix lives:", computer_lives, "/", total_lives)
 	print("Your lives:", player_lives, "/", total_lives)
 	print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
 	print("")
-	# Version 1, to explain array indexing
-	# player_choice = choices[1]
-	# print("index 1 in the choice array is " + player_choice + ", which is paper")
-
 	print("Choose your weapon! If you want to leave, type quit to exit.\n")
+
 	player_choice = input("rock, paper, or scissors? \n")
-	#player_choice now equals TRUE -> It has a value
 
 	if player_choice == "quit" or player_choice == "Quit":
 		print("You chose to quit. Hope to see you soon!")
+		print("")
 		exit()
 
+	print("")
 	print("You chose " + player_choice)
 
-	# this will be the AI choice -> a random pick from the choices array
 	computer_choice = choices[randint(0, 2)]
 
 	print ("The Matrix chose " + computer_choice)
 
 	if computer_choice == player_choice: 
-		print ("What a mind reader, it's a tie!")
+		print ("What a coincidence, it's a tie!")
 
 	elif computer_choice == "rock":
 		if player_choice == "scissors":
 			print("Oh no, you got broken!")
-			#verbose way
-			#player_lives = player_lives - 1
-			#simplified way
 			player_lives -= 1
 		else:
 			print("You're a nice wrap. Well played!")
@@ -87,9 +67,8 @@ while player_choice is False:
 			print("That's a nice cut! Well done.")
 			computer_lives -= 1
 		else:
-			print("You lost (or became the lamest christmas gift)")
+			print("You lost by the lamest christmas gift")
 			player_lives -= 1
-
 
 	elif computer_choice == "scissors":
 		if player_choice == "paper":
@@ -104,12 +83,12 @@ while player_choice is False:
 
 	if computer_lives == 0:
 		winorlose("WON")
-		
-	print("Player lives: ", player_lives)
-	print("Computer lives: ", computer_lives)
+	
+	print("")
+	print("~~| Lives bord |~~")	
+	print("Your lives: ", player_lives)
+	print("Matrix lives: ", computer_lives)
 
-	# map the loop keep running, by setting player_choice back to False
-	# unset, so that our loop condition will evaluate to True
 	player_choice = False
 
 
